c8432025286d997732aa85d782c4e007987f31fc
Laurence Green <lgreen24lg9@gmail.com>
2020-05-06 21:28:00 -0700

    finally finished can't get it to render

--- js/index.js
+++ js/index.js
@@ -1,46 +1,3 @@
-const siteContent = {
-  "nav": {
-    "nav-item-1": "Services",
-    "nav-item-2": "Product",
-    "nav-item-3": "Vision",
-    "nav-item-4": "Features",
-    "nav-item-5": "About",
-    "nav-item-6": "Contact",
-    "img-src": "img/logo.png"
-  },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
-  
-    "img-src": "img/header-img.png"
-  },
-
-  
-  "main-content": {
-    "features-h4":"Features",
-    "features-h4": "Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-  },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
-  },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
-  
-  },
-};
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
@@ -127,5 +84,17 @@ const address = ['Street' ,  'Road' , 'Avenue'];
 let footer = siteContent['contact']['address'].toString();
 
 let firstHalf = footer.substring ( 0, footer.indexOf(address[0]) + address[0] .length);
-let seondHalf = footer. substring (footer.indexOf(address[0] + address[0].length, footer.length)
+let seondHalf = footer. substring (footer.indexOf(address[0] + address[0].length, footer.length));
+
+contactAddress. innerHTML = firstHalf + '</br>' + secondHalf;
+
+const contactNumber = document.querySelector('.contact p:nth-child(3)');
+contactPhone.textContent = siteContent['content']['phone'];
+
+const contactEmail = document.querySelector('.contact p: last-child');
+contactEmail.textContent = siteContent['contact']['email'];
+
+
+const footer = document.querySelector('footer p');
+footer.textContent = siteContent['footer']['copyright'];
 
